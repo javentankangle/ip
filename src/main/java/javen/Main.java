b53919d17830082917671be32b802b6b58a6fd79
javentankangle <javentankangle@gmail.com>
2025-02-17 16:45:57 +0800

    Add title bar to GUI
    Title bar has no name
    
    Title bar should reflect project name so users will know the project name
    
    Let's add the project name to title bar
    
    User will know the application name

--- src/main/java/javen/Main.java
+++ src/main/java/javen/Main.java
@@ -22,6 +22,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Javen");
             fxmlLoader.<MainWindow>getController().setDuke(javen);
             stage.show();
         } catch (IOException e) {
